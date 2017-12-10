dec93216f170c6fced22227e33b270347b5ebce3
Kyle BAMPING <kbamping@e4r4p1.wethinkcode.co.za>
2017-12-10 15:04:03 +0200

    3D modeling done

--- Swifty Protein/DisplayProtein.swift
+++ Swifty Protein/DisplayProtein.swift
@@ -7,14 +7,112 @@
 //
 
 import UIKit
+import SceneKit
 
 class DisplayProtein: UIViewController {
     
-    var Protein: String = ""
+    var Protein: String?
+    var scnView: SCNView!
+    var scnScene: SCNScene!
 
+    @IBOutlet weak var proteinView: UIView!
+    @IBOutlet weak var elementLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.title = Protein
+        
+        scnView = self.proteinView as! SCNView
+        
+        if let ligand = Protein {
+            load(ligand)
+        }
+        
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(DisplayProtein.didTap(_:)))
+        self.view.addGestureRecognizer(tapGR)
+        
+    }
+    
+    override var shouldAutorotate: Bool { return true }
+    override var prefersStatusBarHidden: Bool { return true }
+    
+    /* Function that loads in the ligand info from RCSB */
+    func load(_ ligand: String) {
+        let first = "https://files.rcsb.org/ligands/view/"
+        let last = "_ideal.pdb"
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let url = URL(string: first + ligand + last)
+        let task = URLSession.shared.dataTask(with: url!, completionHandler: {
+            (data, response, error) in
+            
+            if error == nil {
+                if let response = response as? HTTPURLResponse {
+                    
+                    if response.statusCode >= 200 && response.statusCode <= 299 {
+                        
+                        DispatchQueue.main.async(execute: { () -> Void in
+                            let urlContent = NSString(data: data!, encoding: String.Encoding.ascii.rawValue) as NSString!
+                            
+                            var sentenceLines:[String] = []
+                            let string = urlContent as! String
+                            string.enumerateLines { (line, _) in
+                                sentenceLines.append(line)
+                            }
+                            
+                            self.scnView!.scene = PrimitivesScene(file: sentenceLines)
+                            
+                            let cameraNode = SCNNode()
+                            let camera = SCNCamera()
+                            cameraNode.camera = camera
+                            self.scnView!.scene!.rootNode.addChildNode(cameraNode)
+                            cameraNode.position = SCNVector3(x: 0, y: 0, z: 35)
+                            
+                            self.scnView?.scene?.background.contents = UIImage(named: "test.jpg")
+                            self.scnView!.backgroundColor = UIColor.white
+                            self.scnView!.autoenablesDefaultLighting = true
+                            self.scnView!.allowsCameraControl = true
+                            
+                            self.descriptionLabel.text = "Ligand number " + ligand
+                        })
+                        
+                    } else {
+                        
+                        DispatchQueue.main.async(execute: { () -> Void in
+                            let alertController = UIAlertController(title: "Error", message: "Can't load Ligand: \(ligand)", preferredStyle: .alert)
+                            let defaultAction = UIAlertAction(title: "GOTCHA", style: .default, handler: nil)
+                            alertController.addAction(defaultAction)
+                            self.present(alertController, animated: true, completion: nil)
+                        })
+                        
+                        
+                    }
+                }
+                
+            } else {
+                
+                DispatchQueue.main.async(execute: { () -> Void in
+                    let alertController = UIAlertController(title: "Error", message: "Can't load Ligand", preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "GOTCHA", style: .default, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                })
+            }
+        })
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        task.resume()
+    }
+    
+    func didTap(_ tapGR: UITapGestureRecognizer) {
+        let tapPoint = tapGR.location(in: view)
+        let hits = self.scnView!.hitTest(tapPoint, options: nil)
+        
+        if let tappedNode = hits.first?.node {
+            elementLabel.isHidden = false
+            elementLabel.text = "Selected Element: " + tappedNode.ligand.name
+        }
     }
     
     @IBAction func Shared(_ sender: UIBarButtonItem) {
---
+++ Swifty Protein/PrimitivesScene.swift
@@ -0,0 +1,148 @@
+//
+//  PrimitivesScene.swift
+//  Swifty Protein
+//
+//  Created by Kyle BAMPING on 2017/12/09.
+//  Copyright © 2017 Dillon MATHER. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class PrimitivesScene: SCNScene {
+    
+    var file : [String]?
+    var atoms : [SCNNode] = []
+    
+    init(file: [String]) {
+        super.init()
+        
+        let sphereGeometry = SCNSphere(radius: 0.5)
+        let sphereNode = SCNNode(geometry: sphereGeometry)
+        self.atoms.append(sphereNode)
+        
+        for line in file {
+            var fullNameArr = line.components(separatedBy: " ")
+            fullNameArr = fullNameArr.filter { $0 != "" }
+            if fullNameArr.count > 0 {
+                
+                if fullNameArr[0] == "ATOM" {
+                    
+                    let secondSphereGeometry = SCNSphere(radius: 0.3)
+                    let secondSphereNode = SCNNode(geometry: secondSphereGeometry)
+                    secondSphereNode.position = SCNVector3(x: Float(fullNameArr[6])!, y: Float(fullNameArr[7])!, z: Float(fullNameArr[8])!)
+                    secondSphereNode.ligand.name = fullNameArr[11]
+                    CPKcoloring(secondSphereGeometry, color: fullNameArr[11])
+                    self.rootNode.addChildNode(secondSphereNode)
+                    self.atoms.append(secondSphereNode)
+                    
+                } else if fullNameArr[0] == "CONECT" {
+                    
+                    let from : Int = Int(fullNameArr[1])!
+                    let to = fullNameArr[2..<fullNameArr.count]
+                    for elem in to {
+                        let line = self.lineBetweenNodeA(self.atoms[from], nodeB: self.atoms[Int(elem)!])
+                        self.rootNode.addChildNode(line)
+                    }
+                    
+                }
+                
+            }
+        }
+        
+    }
+    
+    func CPKcoloring(_ gem: SCNSphere, color: String) {
+        
+        switch color {
+        case "H":
+            gem.firstMaterial?.diffuse.contents = UIColor.white
+        case "C":
+            gem.firstMaterial?.diffuse.contents = UIColor.black
+        case "N":
+            gem.firstMaterial?.diffuse.contents = UIColor(red:0.00, green:0.14, blue:0.49, alpha:1.0)
+        case "O":
+            gem.firstMaterial?.diffuse.contents = UIColor.red
+        case "F", "Cl":
+            gem.firstMaterial?.diffuse.contents = UIColor.green
+        case "Br":
+            gem.firstMaterial?.diffuse.contents = UIColor(red:0.49, green:0.00, blue:0.00, alpha:1.0)
+        case "I":
+            gem.firstMaterial?.diffuse.contents = UIColor(red:0.37, green:0.00, blue:0.49, alpha:1.0)
+        case "He", "Ne", "Ar", "Xe", "Kr":
+            gem.firstMaterial?.diffuse.contents = UIColor(red:0.22, green:0.94, blue:0.99, alpha:1.0)
+        case "P":
+            gem.firstMaterial?.diffuse.contents = UIColor.orange
+        case "S":
+            gem.firstMaterial?.diffuse.contents = UIColor.yellow
+        case "B":
+            gem.firstMaterial?.diffuse.contents = UIColor(red:0.99, green:0.73, blue:0.51, alpha:1.0)
+        case "Li", "Na", "K", "Rb", "Cs", "Fr":
+            gem.firstMaterial?.diffuse.contents = UIColor.purple
+        case "Be", "Mg", "Ca", "Sr", "Ba", "Ra":
+            gem.firstMaterial?.diffuse.contents = UIColor(red:0.05, green:0.33, blue:0.02, alpha:1.0)
+        case "Ti":
+            gem.firstMaterial?.diffuse.contents = UIColor.gray
+        case "Fe":
+            gem.firstMaterial?.diffuse.contents = UIColor.orange
+        default:
+            gem.firstMaterial?.diffuse.contents = UIColor(red:1.00, green:0.00, blue:0.60, alpha:1.0)
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func lineBetweenNodeA(_ nodeA: SCNNode, nodeB: SCNNode) -> SCNNode {
+        let positions: [Float32] = [nodeA.position.x, nodeA.position.y, nodeA.position.z, nodeB.position.x, nodeB.position.y, nodeB.position.z]
+        let positionData = Data(bytes: UnsafePointer(positions), count: MemoryLayout<Float32>.size*positions.count)
+        let indices: [Int32] = [0, 1]
+        let indexData = Data(bytes: UnsafePointer(indices), count: MemoryLayout<Int32>.size * indices.count)
+        
+        let source = SCNGeometrySource(data: positionData, semantic: SCNGeometrySource.Semantic.vertex, vectorCount: indices.count, usesFloatComponents: true, componentsPerVector: 3, bytesPerComponent: MemoryLayout<Float32>.size, dataOffset: 0, dataStride: MemoryLayout<Float32>.size * 3)
+        let element = SCNGeometryElement(data: indexData, primitiveType: SCNGeometryPrimitiveType.line, primitiveCount: indices.count, bytesPerIndex: MemoryLayout<Int32>.size)
+        
+        let line = SCNGeometry(sources: [source], elements: [element])
+        line.firstMaterial?.diffuse.contents = UIColor.black
+        return SCNNode(geometry: line)
+    }
+    
+}
+
+func associatedObject<ValueType: AnyObject>(
+    _ base: AnyObject,
+    key: UnsafePointer<UInt8>,
+    initialiser: () -> ValueType)
+    -> ValueType {
+        if let associated = objc_getAssociatedObject(base, key)
+            as? ValueType { return associated }
+        let associated = initialiser()
+        objc_setAssociatedObject(base, key, associated,
+                                 .OBJC_ASSOCIATION_RETAIN)
+        return associated
+}
+func associateObject<ValueType: AnyObject>(
+    _ base: AnyObject,
+    key: UnsafePointer<UInt8>,
+    value: ValueType) {
+    objc_setAssociatedObject(base, key, value,
+                             .OBJC_ASSOCIATION_RETAIN)
+}
+
+class Ligand {
+    var name = ""
+}
+
+private var ligandKey: UInt8 = 0
+
+extension SCNNode {
+    var ligand: Ligand {
+        get {
+            return associatedObject(self, key: &ligandKey)
+            { return Ligand() }
+        }
+        set { associateObject(self, key: &ligandKey, value: newValue) }
+    }
+}
